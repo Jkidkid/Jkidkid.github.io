fbd958b82890ef5db951b45690a44bd7c6ff61cc
Simonkyh <Simon.gribert@student.kyh.se>
2018-02-12 12:07:29 +0100

    create tab function

---
+++ script/tabs.js
@@ -0,0 +1,9 @@
+let profile = document.getElementById('profile'),
+    start = document.getElementById('start'),
+    groupScore = document.getElementById('groupScore');
+function showdiv(id) {
+  profile.style.display = "none";
+  start.style.display = "none";
+  groupScore.style.display = "none";
+  document.getElementById(id).style.display = 'flex';
+}
