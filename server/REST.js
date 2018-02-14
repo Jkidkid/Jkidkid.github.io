cf2f7e1bad87e146b467b773a1ce4d07a4a3f7b3
Mikael Gustafsson <mikael.gustafsson@student.kyh.se>
2018-02-14 09:33:50 +0100

    minor change

--- server/REST.js
+++ server/REST.js
@@ -41,8 +41,8 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
   // Test function to update db post
   router.get("/updateEmail/:id",function(req,res){
     res.setHeader('Access-Control-Allow-Origin', '*');
-        var query = `UPDATE user SET email = "micke@example.se" WHERE id = ${req.params.id}`;
-        var table = ["uid", "user","id",req.params.id];
+    var query = `UPDATE ?? SET ?? = "ElinKviberg@example.se" WHERE ?? = ${req.params.id}`;
+    var table = ["user", "email","id"];
         query = mysql.format(query,table);
         connection.query(query,function(err,rows){
           if(err) {
