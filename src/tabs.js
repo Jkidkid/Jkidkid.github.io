0e7e08fc5db87a3e998b2b8b260eea8d0bc7f1d9
Joakim Luong <joakimluong@Joakims-MacBook-Pro.local>
2018-02-14 10:43:42 +0100

    comment out showdiv group.style.display

--- src/tabs.js
+++ src/tabs.js
@@ -1,8 +1,10 @@
 let profile = document.getElementById('profile'),
     start = document.getElementById('start'),
-<<<<<<< HEAD
-    groupScore = document.getElementById('groupScore');
-
+    groupScore = document.getElementById('groupScore'),
+    Sgroup = document.getElementById('score-group'),
+    Splayer = document.getElementById('score-player'),
+    containerC = document.getElementById('content-container'),
+    createGroup = document.getElementById('create-group');
 
 
 
@@ -10,28 +12,14 @@ function showdiv(id) {
     profile.style.display = "none";
     start.style.display = "none";
     groupScore.style.display = "none";
+    // group.style.display = "none";
     document.getElementById(id).style.display = 'flex';
-=======
-    groupScore = document.getElementById('groupScore'),
-    group = document.getElementById('group'),
-    Sgroup = document.getElementById('score-group'),
-    Splayer = document.getElementById('score-player'),
-    containerC = document.getElementById('content-container'),
-    createGroup = document.getElementById('create-group');
-
-function showdiv(id) {
-  profile.style.display = "none";
-  start.style.display = "none";
-  groupScore.style.display = "none";
-  group.style.display = "none";
-  document.getElementById(id).style.display = 'flex';
 }
+
 function showdivv(id){
   createGroup.style.display = "none";
   containerC.style.display = "none";
   Sgroup.style.display = "none";
   Splayer.style.display = "none";
   document.getElementById(id).style.display = 'flex';
-
->>>>>>> usernav
 }
